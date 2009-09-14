7d523ec827f94c3ba58cf546fa5b7891be36e37c
fathzer <fathzer@github.com>
2009-09-14 16:35:05 +0000

    Add proxy support

--- src/net/yapbam/update/UpdateInformation.java
+++ src/net/yapbam/update/UpdateInformation.java
@@ -2,10 +2,14 @@ package net.yapbam.update;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.Properties;
+
+import net.yapbam.ihm.Preferences;
 
 public class UpdateInformation {
 	private int errorCode;
@@ -13,8 +17,9 @@ public class UpdateInformation {
 	private URL updateURL;
 	
 	UpdateInformation (URL url) throws UnknownHostException, IOException {
-		//TODO Connect via a proxy : http://forums.sun.com/thread.jspa?threadID=500360
-		HttpURLConnection ct = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY);
+		InetAddress host = Preferences.INSTANCE.getHttpProxyHost();
+		Proxy proxy = host==null?Proxy.NO_PROXY:new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, Preferences.INSTANCE.getHttpProxyPort()));
+		HttpURLConnection ct = (HttpURLConnection) url.openConnection(proxy);
 		errorCode = ct.getResponseCode();
 		if (errorCode==HttpURLConnection.HTTP_OK) {
 			Properties p = new Properties();
