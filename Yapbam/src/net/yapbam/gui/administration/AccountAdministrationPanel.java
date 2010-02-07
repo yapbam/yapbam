6763d9ab94aba22b3725dbef9cc6f6f039fab623
fathzer <fathzer@763f1f3b-c17a-45ed-94d4-184818e67969>
2010-02-07 18:06:38 +0000

    Added some Portuguese wordings

--- Yapbam/src/net/yapbam/gui/administration/AccountAdministrationPanel.java
+++ Yapbam/src/net/yapbam/gui/administration/AccountAdministrationPanel.java
@@ -98,7 +98,7 @@ public class AccountAdministrationPanel extends JPanel implements AbstractAdmini
 	private ModeListPanel getModeListPanel() {
 		if (modeListPanel == null) {
 			modeListPanel = new AdministrationModeListPanel(data);
-			modeListPanel.setBorder(BorderFactory.createTitledBorder(null, "Modes de paiement", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
+			modeListPanel.setBorder(BorderFactory.createTitledBorder(null, LocalizationData.get("AccountDialog.modes.border.title"), TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.BOLD, 12), new Color(51, 51, 51)));
 		}
 		return modeListPanel;
 	}
