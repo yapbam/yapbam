7ed4f2e9697d2ed6cf485439a647cbab1d803899
fathzer <fathzer@763f1f3b-c17a-45ed-94d4-184818e67969>
2011-09-25 16:25:14 +0000

    version 0.9.3

--- Yapbam/testSrc/net/yapbam/junit/WidgetTest.java
+++ Yapbam/testSrc/net/yapbam/junit/WidgetTest.java
@@ -2,10 +2,12 @@ package net.yapbam.junit;
 
 import java.awt.BorderLayout;
 import java.util.Date;
+import java.util.Locale;
 
 import javax.swing.JDialog;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import net.yapbam.gui.widget.AmountWidget;
 import net.yapbam.gui.widget.DateWidgetPanel;
@@ -13,7 +15,7 @@ import net.yapbam.gui.widget.DateWidgetPanel;
 public class WidgetTest {
 	private PropertyChangeSpy listener = new PropertyChangeSpy();
 	
-	@Test
+/*	@Test
 	public void test() {
 		DateWidgetPanel datePanel = new DateWidgetPanel();
 		datePanel.addPropertyChangeListener(DateWidgetPanel.DATE_PROPERTY, listener);
@@ -36,5 +38,20 @@ public class WidgetTest {
 //		assertNotNull(listener.lastEvent);
 //		assertNull(listener.lastEvent.getOldValue());
 //		assertEquals(listener.lastEvent.getNewValue(), 0);
+	}*/
+	
+	@Test
+	public void testAmountWidget() {
+		testAmountWidget(new Locale("FR","fr"), "5,25", 5.25);
+		testAmountWidget(new Locale("FR","be"), "5,25", 5.25);
+		testAmountWidget(new Locale("PT","br"), "5,25", 5.25);
+		testAmountWidget(new Locale("EN","us"), "5.25", 5.25);
+	}
+
+	private void testAmountWidget(Locale locale, String str, double value) {
+		AmountWidget widget = new AmountWidget(locale);
+		widget.setText(str);
+		assertTrue (widget.getValue()==value);
+//		System.out.println (locale+" is ok");
 	}
 }
