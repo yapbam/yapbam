59a65c0d53c712766812344506e81d33373c2b22
fathzer <fathzer@763f1f3b-c17a-45ed-94d4-184818e67969>
2011-10-23 14:49:33 +0000

    version 0.9.6

--- Yapbam/srcTests/net/yapbam/update/GetHashAndSize.java
+++ Yapbam/srcTests/net/yapbam/update/GetHashAndSize.java
@@ -11,8 +11,8 @@ public class GetHashAndSize {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String release = "0.9.5";
-		String date = "16/10/2011";
+		String release = "0.9.6";
+		String date = "23/10/2011";
 		System.out.println ("lastestRelease="+release+" ("+date+")");
 		System.out.println ("updateURL=http://sourceforge.net/project/platformdownload.php?group_id=276272");
 		System.out.println ();
