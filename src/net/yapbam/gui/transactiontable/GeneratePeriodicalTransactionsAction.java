b1bd01b3fec6db0ac6c28234b0ddeac9767ae426
fathzer <fathzer@github.com>
2011-12-27 10:52:26 +0000

    adds a button to generate periodical transactions in the transactions table

--- src/net/yapbam/gui/transactiontable/GeneratePeriodicalTransactionsAction.java
+++ src/net/yapbam/gui/transactiontable/GeneratePeriodicalTransactionsAction.java
@@ -2,16 +2,21 @@ package net.yapbam.gui.transactiontable;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.text.MessageFormat;
+import java.util.Date;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.Icon;
 
 import net.yapbam.data.FilteredData;
+import net.yapbam.data.Transaction;
 import net.yapbam.data.event.DataEvent;
 import net.yapbam.data.event.DataListener;
 import net.yapbam.data.event.EverythingChangedEvent;
 import net.yapbam.data.event.PeriodicalTransactionsAddedEvent;
 import net.yapbam.data.event.PeriodicalTransactionsRemovedEvent;
+import net.yapbam.gui.IconManager;
 import net.yapbam.gui.LocalizationData;
 import net.yapbam.gui.dialogs.GeneratePeriodicalTransactionsDialog;
 import net.yapbam.gui.util.AbstractDialog;
@@ -43,16 +48,25 @@ public class GeneratePeriodicalTransactionsAction extends AbstractAction {
 
 	private void refreshEnabled(boolean forced) {
 		boolean enabled = data.getGlobalData().getPeriodicalTransactionsNumber()!=0;
-		if (forced || (enabled!=this.isEnabled())) {
-			setEnabled(enabled);
-			StringBuilder toolTip = new StringBuilder();
-			toolTip.append("<html>");
-			toolTip.append(LocalizationData.get("MainMenu.Transactions.Periodical.ToolTip")); //$NON-NLS-1$
-			if (!enabled) {
-				toolTip.append("<br>").append("Damned, it is grayed out");
+		setEnabled(enabled);
+		StringBuilder toolTip = new StringBuilder();
+		toolTip.append("<html>"); //$NON-NLS-1$
+		toolTip.append(LocalizationData.get("MainMenu.Transactions.Periodical.ToolTip")); //$NON-NLS-1$
+		Icon icon = null;
+		if (!enabled) {
+			toolTip.append("<br>").append(LocalizationData.get("MainMenu.Transactions.Periodical.disabled.tooltip.line1")); //$NON-NLS-1$ //$NON-NLS-2$
+			toolTip.append("<br>").append(MessageFormat.format(LocalizationData.get("MainMenu.Transactions.Periodical.disabled.tooltip.line2"), //$NON-NLS-1$ //$NON-NLS-2$
+					LocalizationData.get("PeriodicalTransactionManager.title"),LocalizationData.get("AdministrationPlugIn.title"))); //$NON-NLS-1$ //$NON-NLS-2$
+		} else {
+			Transaction[] transactions = data.getGlobalData().generateTransactionsFromPeriodicals(new Date());
+			if (transactions.length!=0) {
+				toolTip.append("<br>").append(LocalizationData.get("GeneratePeriodicalTransactionsDialog.alert")); //$NON-NLS-1$ //$NON-NLS-2$
+				icon = IconManager.ALERT;
 			}
-			toolTip.append("</html>");
-			putValue(SHORT_DESCRIPTION, toolTip.toString());
 		}
+		toolTip.append("</html>"); //$NON-NLS-1$
+		putValue(SHORT_DESCRIPTION, toolTip.toString());
+		this.putValue(SMALL_ICON, icon);
+		this.putValue(LARGE_ICON_KEY, icon);
 	}
 }
