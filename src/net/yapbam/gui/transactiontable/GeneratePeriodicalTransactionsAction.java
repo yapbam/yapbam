0fcbb3bed14ecd3d477b89db4e7c7d10b7049bae
fathzer <fathzer@github.com>
2012-05-12 11:04:44 +0000

    version 0.11.1

--- src/net/yapbam/gui/transactiontable/GeneratePeriodicalTransactionsAction.java
+++ src/net/yapbam/gui/transactiontable/GeneratePeriodicalTransactionsAction.java
@@ -28,8 +28,8 @@ public class GeneratePeriodicalTransactionsAction extends AbstractAction {
 	public GeneratePeriodicalTransactionsAction(FilteredData filteredData, boolean isMenu) {
 		super(LocalizationData.get("MainMenu.Transactions.Periodical")); //$NON-NLS-1$
 		if (isMenu) putValue(Action.MNEMONIC_KEY, (int) LocalizationData.getChar("MainMenu.Transactions.Periodical.Mnemonic")); //$NON-NLS-1$
+		this.data = filteredData;
 		if (data!=null) {
-			this.data = filteredData;
 			filteredData.getGlobalData().addListener(new DataListener() {
 				@Override
 				public void processEvent(DataEvent event) {
