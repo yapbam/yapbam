1f528d29666127b02eea72b6da95c06ccf46cc5a
marcoM32 <magliano32@gmail.com>
2021-12-13 15:06:19 +0100

    Fix for issue #14

--- testSrc/net/yapbam/gui/TranslationsTest.java
+++ testSrc/net/yapbam/gui/TranslationsTest.java
@@ -1,6 +1,6 @@
 package net.yapbam.gui;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,7 +8,9 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -19,24 +21,31 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import com.fathzer.soft.ajlib.utilities.StringUtils;
+
 public class TranslationsTest {
 
 	@Test
 	public void test() throws IOException {
-		Map<String, List<String>> missing = getMissing("src/Resources");
-		assertTrue("Missing resources: "+missing, missing.isEmpty());
+		final String[] path = {
+			"src/Resources",
+			"src/net/yapbam/gui/tools/messages"
+		};
 		
-		missing = getMissing("src/net/yapbam/gui/tools/messages");
-		assertTrue("Missing resources: "+missing, missing.isEmpty());	}
+		Map<String, List<String>> missing = null;
+		for(String location : path) {
+			missing = getMissing(location);
+			assertTrue("Missing resources: " + missing, missing.isEmpty());
+		}
+		
+		for(String location : path) {
+			missing = getEpoorlyFormatted(location);
+			assertTrue("Wrong resources: " + missing, missing.isEmpty());
+		}
+	}
 
 	private Map<String, List<String>> getMissing(String path) throws IOException {
 		final File f = new File(path);
-		final String[] translations = f.getParentFile().list(new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String name) {
-				return name.startsWith(f.getName()+"_") && name.endsWith(".properties");
-			}
-		});
 		
 		final Set<String> known = new HashSet<String>();
 		final Enumeration<String> knownEnum = getKeys(new File(path+".properties"));
@@ -45,7 +54,7 @@ public class TranslationsTest {
 		}
 
 		Map<String, List<String>> result = new HashMap<String, List<String>>();
-		for (String translation : translations) {
+		for (String translation : getFiles(f)) {
 			final int index = f.getName().length();
 			final String lng = translation.substring(index+1, index+3);
 			final Enumeration<String> keys = getKeys(new File(f.getParent(),translation));
@@ -59,6 +68,23 @@ public class TranslationsTest {
 		return result;
 	}
 
+	private Map<String, List<String>> getEpoorlyFormatted(String path) throws IOException {
+		final File f = new File(path);
+
+		Map<String, List<String>> result = new HashMap<String, List<String>>();
+		for (String translation : getFiles(f)) {
+			MessageFormat messageFormat = new MessageFormat(StringUtils.EMPTY);
+			for(String pattern : getValues(new File(f.getParent(),translation))) {
+				try {
+					messageFormat.applyPattern(pattern);
+				} catch(IllegalArgumentException ex) {
+					add(result, translation, pattern);
+				}
+			}
+		}
+		return result;
+	}
+	
 	private void add(Map<String, List<String>> result, String key, String lng) {
 		List<String> languages = result.get(key);
 		if (languages==null) {
@@ -89,4 +115,26 @@ public class TranslationsTest {
 			}
 		};
 	}
+	
+	private String[] getValues(File translationFile) throws FileNotFoundException, IOException {
+		final Properties content = new Properties();
+		final InputStream stream = new FileInputStream(translationFile);
+		try {
+			content.load(stream);
+		} finally {
+			stream.close();
+		}
+		final Collection<Object> values = content.values();
+		return values.toArray(new String[values.size()]);
+	}
+	
+	private String[] getFiles(final File path) {
+		return path.getParentFile().list(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.startsWith(path.getName()+"_") && name.endsWith(".properties");
+			}
+		});
+	}
+	
 }
